4e98d04bee205e720edbf496d3513a7a518000fe
jjochen <jjochen@a36cb7eb-b833-0410-929d-9d6c17de6d6e>
2008-02-13 13:39:44 +0000

    back link on info page
    git-svn-id: svn://svn.code.sf.net/p/prechacthis/code/trunk/src@330 a36cb7eb-b833-0410-929d-9d6c17de6d6e

--- info.php
+++ info.php
@@ -6,13 +6,16 @@
 	<link rel="stylesheet" type="text/css" href="./css/prechacthis.css">
 </head>
 <body>
-<div id="beta">this page is still beta!</div>
 <?php
 $debug = false;
 
 if ($_REQUEST){
 	if($_GET["debug"]=="on") $debug = true;
 
+	if($_GET["back"]) {
+		$back_url = ".". rawurldecode($_GET["back"]);
+		echo "<p class="back"><a href='".$back_url."'>back to results</a></p>";
+	}
 	
 	if($_GET["pattern"] && $_GET["persons"]) {
 	    echo "\n<table align='center' cellpadding='0'><tr><td><div align='center'>\n";
@@ -36,6 +39,10 @@ if ($_REQUEST){
 		echo "\n</div></td></tr></table>";
 	}else{
 		echo "<p>pattern or number of jugglers not specified :-(</p>";
+	}
+	
+	if($_GET["back"]) {
+		echo "<p class="back"><a href='".$back_url."'>back to results</a></p>";
 	}
 }
 ?>
