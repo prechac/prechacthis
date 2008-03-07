760b1b3740cdd512e8de02f0e58d0656b7f656b8
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2008-03-07 01:10:59 +0000

    new documented example
    git-svn-id: svn://svn.code.sf.net/p/prechacthis/code/trunk@374 a36cb7eb-b833-0410-929d-9d6c17de6d6e

--- src/index.php
+++ src/index.php
@@ -51,10 +51,10 @@ if ($_REQUEST){
 			$errorlogfile = tempnam("/tmp", "siteswap");
 	
 			$plquery  = "swipl -q "
-					  . "-L5M " // local stack size to 10 MB
-					  . "-G12M " // global stack size to 10 MB
-					  . "-T5M " // trail stack size to 10 MB
-					  . "-A2M " // argutment stack size to 5 MB
+					  . "-L5M " // local stack size to 5 MB
+					  . "-G12M " // global stack size to 12 MB
+					  . "-T5M " // trail stack size to 5 MB
+					  . "-A2M " // argutment stack size to 2 MB
 			          . "-f " . dirname($_SERVER["SCRIPT_FILENAME"]) . "/pl/siteswap.pl "
 			          . "-g \"allSiteswaps("
 			          . $_GET["persons"] . ", "
@@ -231,7 +231,7 @@ echo "<form action='./index.php' method='get'>
   </tr>
   <tr>
    <td class='lable'>&nbsp;</td>
-   <td class='input'><a href='./index.php?persons=2&amp;objects=2%3B3%3B5&amp;lengths=4%3B5&amp;max=4&amp;multiplex=0&amp;passesmin=1&amp;passesmax=&amp;jugglerdoes=1+1.5p%2C+1%3B+3p&amp;clubdoes=1.5p+1%3B+1p+3p%3B+3+3p&amp;react=4+1%3B+3p+0%3B+1.5p+1&amp;doku=on&amp;exclude=4+0'>documented example</a></td>
+   <td class='input'><a href='./index.php?persons=2&objects=4%3B5&lengths=4%3B5%3B6&max=4&passesmin=1&passesmax=4&jugglerdoes=3+%283p+or+4p%29+or+3+%3Fp&exclude=0+and+1+1+and+2+2&clubdoes=%281+or+2%29+%281p+or+1.5p%29+or+%281p+or+1.5p%29+%281+or+2%29&react=2p+3+or+4p+4+or+3+1.5p&results=42&doku=on'>documented example</a></td>
   </tr>
   <tr>
    <td class='lable'>&nbsp;</td>
@@ -246,27 +246,6 @@ echo "<form action='./index.php' method='get'>
 </form>
 ";
 
-/*function correctLengths($seqs){
-	if ("" == $seqs){
-		return "[3]"; //default :-)
-	}
-	$outstring = "[";
-	$lengths = explode(",", $seqs);
-	foreach ($lengths as $length) {
-		$outstring .= correctLength(trim($length)) . ",";
-	}
-	$outstring = substr($outstring, 0, strlen($outstring) -1); //take off last comma
-	$outstring .= "]";
-	return $outstring;
-}*/
-
-
-function correctLongPasses($seq){
-	$seq = ereg_replace('([0-9_.]+)(p)([0-9_.]+)', 'p(\\1*\\3)', $seq);
-	$seq = ereg_replace('(p\([0-9_.]+)(,)([0-9_.]+\))', '\\1*\\3', $seq);
-	$seq = ereg_replace('(p\([0-9_.]+)(,)([0-9_.]+)(,)([0-9_.]+\))', '\\1*\\3*\\5', $seq);
-	return $seq;
-}
 
 function correctPasses($passes){
 	if (!is_numeric($passes)){
@@ -275,92 +254,20 @@ function correctPasses($passes){
 	return $passes;
 }
 
-function correctMultiplexes($seq){
-	$seq = ereg_replace('(\[[0-9pP_]+)(,|;)([0-9pP_]+\])', '\\1*\\3', $seq);
-	return $seq;
-}
-function correctSeqsSemicolon($seqs){
-    $seqs = correctMultiplexes($seqs);
-	$seqs = correctLongPasses($seqs);
-	$outstring = "[";
-	$seqs = str_replace('or', ';', $seqs);
-	$seqs = explode(";", $seqs);
-	foreach ($seqs as $seq) {
-		$outstring .= correctSeqs($seq) . ",";
-	}
-	$outstring = substr($outstring, 0, strlen($outstring) -1); //take off last comma
-	$outstring .= "]";
-	return $outstring;
-}
-
-function correctSeqs($seqs){
-	if ("" == $seqs){
-		return "[]"; //pass to prolog as empty
-	}
-	
-	$outstring = "[";
-	$seqs = str_replace('and', ',', $seqs);
-	$seqs = explode(",", $seqs);
-	foreach ($seqs as $seq) {
-		$outstring .= correctSeq($seq) . ",";
-	}
-	$outstring = substr($outstring, 0, strlen($outstring) -1); //take off last comma
-	$outstring .= "]";
-	return $outstring;
-}
-
-function correctSeq($seq){
-	$outstring = "[";
-	$seq = explode(" ", trim($seq));
-	foreach ($seq as $throw) {
-		$throw = trim($throw);
-		if (substr($throw, 0, 1) == "["){
-			$outstring .= convert_multiplex($throw);
-		} else {
-			$outstring .= convert_throw($throw);
-		}
-		$outstring .= ",";
-	}
-	$outstring = substr($outstring, 0, strlen($outstring) -1); //take off last comma
-	$outstring .= "]";
-	return $outstring;
-}
-
-
-function convert_multiplex($throw){
-	$inner_multiplex = substr($throw,1,strlen($throw)-2);
-	$multiplex = explode("*", $inner_multiplex);
-	$outstring .= "[";
-	foreach ($multiplex as $mthrow){
-		$outstring .= convert_throw($mthrow);
-		$outstring .= ",";
-	}
-	$outstring = substr($outstring, 0, strlen($outstring) -1); //take off last comma
-	$outstring .= "]";
-	return $outstring;
-}
-
-function convert_throw($throw){
-	$throw = trim($throw);
-	$throw = strtr($throw, '*', ',' );
-	if (!(is_numeric($throw) || $throw == "_" || substr($throw, 0, 2) == "p(")) {
-		$throw = "p(" . substr($throw, 0, strlen($throw)-1) . ")";
-	}
-	return $throw;
-}
 
 function doku($doku_flag){
 if ($doku_flag){
 $doku["head"]     = "   <td class='head'>Meaning</td>\n";
 $doku["persons"]  = "   <td class='sample'>2 persons</td>\n";
-$doku["objects"]  = "   <td class='sample'>2, 3 or 5 objects</td>\n";
-$doku["length"]   = "   <td class='sample'>period length 4 or 5. max is 6. takes a while</td>\n";
+$doku["objects"]  = "   <td class='sample'>4 or 5 objects</td>\n";
+$doku["length"]   = "   <td class='sample'>period length 4, 5 or 6.</td>\n";
 $doku["max"]      = "   <td class='sample'>highest throw is a 4</td>\n";
 $doku["multiplex"]= "   <td class='sample'>no multiplexes</td>\n";
-$doku["exclude"]  = "   <td class='sample'>exclude pattern that have a 4 0</td>\n";
-$doku["contain"]  = "   <td class='sample'>find pattern that contain:<br>1 1.5p and a 1<br>OR<br>a 3p</td>\n";
-$doku["clubdoes"] = "   <td class='sample'>a club does a 1.5p and then a 1<br>OR<br> a 1p or a 3 followd by a 3p</td>\n";
-$doku["react"]    = "   <td class='sample'>throw a 1 underneath a 4<br>OR<br>before catching a 3p the hand is empty<br>OR<br>react with a 1 to a 1.5p</td>\n";
+$doku["exclude"]  = "   <td class='sample'>exclude patterns that contain<br>a 0, a 1 1 or a 2 2</td>\n";
+$doku["contain"]  = "   <td class='sample'>find patterns that contain<br>3 3p or 3 4p or 3 followed by a pass.</td>\n";
+$doku["clubdoes"] = "   <td class='sample'>a club does a 1 or a 2 followed by a 1p or a 1.5p<br>or the other way round.</td>\n";
+$doku["react"]    = "   <td class='sample'>throw a 3 underneath an arriving 2p<br>or before catching a 4p throw a 4<br>or react with a 1.5p to a 3.</td>\n";
+$doku["results"]  = "   <td class='sample'>don't show more than 42 results.</td>\n";
 }else{$doku="";}
 return $doku;
 }
