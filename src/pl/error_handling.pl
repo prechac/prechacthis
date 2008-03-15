7c922ec25d247e7ee99c0f674e63fda32390941a
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2008-03-15 00:18:06 +0000

    deleted most of error_handling.pl
    git-svn-id: svn://svn.code.sf.net/p/prechacthis/code/trunk@385 a36cb7eb-b833-0410-929d-9d6c17de6d6e

--- src/pl/error_handling.pl
+++ src/pl/error_handling.pl
@@ -1,9 +1,3 @@
-initErrorHandling :- 
-	retractall(prechac_error(_)),!.
+
 	
-raise_error(String) :-
-	asserta(prechac_error(String)),
-	!,fail.
-	
-write_errors(Format) :-
-	forall(prechac_error(Error), format(Format, [Error])).
+write_exception(_Format).
