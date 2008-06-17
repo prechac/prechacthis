6267b6cc028823838e0e3b308a178609ecaee5cb
flowma <git@flomar.de>
2008-06-17 11:23:12 +0000

    prechantu 0.2
    git-svn-id: svn://svn.code.sf.net/p/prechacthis/code/trunk@412 a36cb7eb-b833-0410-929d-9d6c17de6d6e

--- distrib/ubuntu-customized/export_relevant_tags.sh
+++ distrib/ubuntu-customized/export_relevant_tags.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+CHROOT=$1
 CHROOTwww=$CHROOT/var/www
 PT_DIR=$CHROOTwww/prechacthis
-echo "sudo chmod a+rw $CHROOTwww"
-sudo chmod a+rw $CHROOTwww
+REPO="https://prechacthis.svn.sourceforge.net/svnroot/prechacthis"
+
 mkdir -p $CHROOT/var/www/prechacthis
-svn export ../../../tags/1.1 $PT_DIR/stable
-svn export ../../../tags/multiplex $PT_DIR/multiplex
-svn export ../../../trunk/src $PT_DIR/beta
+svn export $REPO/tags/1.1 $PT_DIR/stable
+svn export $REPO/tags/multiplex $PT_DIR/multiplex
+svn export $REPO/trunk/src $PT_DIR/beta
 chmod -R a+r $PT_DIR
 chmod a+x `find $PT_DIR -type d`
--- distrib/ubuntu-customized/patch_remaster_root.sh
+++ distrib/ubuntu-customized/patch_remaster_root.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-export CHROOT=~/tmp/remaster-root
-./export_relevant_tags.sh
-echo startx >> $CHROOT/etc/skel/.bashrc
+USER=$(whoami)
+
+CHROOT=/home/$USER/tmp/remaster-root
+
+echo "chmod -R a+rw $CHROOT/etc $CHROOT/var"
+sudo chmod -R a+rw $CHROOT/etc $CHROOT/var
+
+sudo ./export_relevant_tags.sh $CHROOT
+
+sudo echo startx >> $CHROOT/etc/skel/.bashrc
+
+AUTOSTART_DIR=$CHROOT/etc/skel/.config/Autostart
+sudo mkdir -p $AUTOSTART_DIR
+sudo cp prechacthis.desktop $AUTOSTART_DIR
+
+sudo chmod -R a+rw $CHROOT/etc $CHROOT/var
