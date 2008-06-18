f812e4d2c7f7c01d908b965280a1b53ae4a4f6e2
flowma <git@flomar.de>
2008-06-18 20:56:34 +0000

    neu
    git-svn-id: svn://svn.code.sf.net/p/prechacthis/code/trunk@413 a36cb7eb-b833-0410-929d-9d6c17de6d6e

--- distrib/ubuntu-customized/export_relevant_tags.sh
+++ distrib/ubuntu-customized/export_relevant_tags.sh
@@ -8,5 +8,6 @@ mkdir -p $CHROOT/var/www/prechacthis
 svn export $REPO/tags/1.1 $PT_DIR/stable
 svn export $REPO/tags/multiplex $PT_DIR/multiplex
 svn export $REPO/trunk/src $PT_DIR/beta
+svn export $REPO/trunk/distrib/ubuntu-customized/index_releases.html $PT_DIR/index.html
 chmod -R a+r $PT_DIR
 chmod a+x `find $PT_DIR -type d`
--- distrib/ubuntu-customized/patch_remaster_root.sh
+++ distrib/ubuntu-customized/patch_remaster_root.sh
@@ -4,14 +4,14 @@ USER=$(whoami)
 CHROOT=/home/$USER/tmp/remaster-root
 
 echo "chmod -R a+rw $CHROOT/etc $CHROOT/var"
-sudo chmod -R a+rw $CHROOT/etc $CHROOT/var
+sudo chmod -R a+rw $CHROOT/etc/skel $CHROOT/var/www
 
 sudo ./export_relevant_tags.sh $CHROOT
 
 sudo echo startx >> $CHROOT/etc/skel/.bashrc
 
-AUTOSTART_DIR=$CHROOT/etc/skel/.config/Autostart
+AUTOSTART_DIR=$CHROOT/etc/skel/.config/autostart
 sudo mkdir -p $AUTOSTART_DIR
 sudo cp prechacthis.desktop $AUTOSTART_DIR
 
-sudo chmod -R a+rw $CHROOT/etc $CHROOT/var
+sudo chmod -R a+rw $CHROOT/etc/skel $CHROOT/var/www
