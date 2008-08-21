f6c64a4aad7cad5192ff6c715adb2892babc5f1f
jjochen <jjochen@a36cb7eb-b833-0410-929d-9d6c17de6d6e>
2008-08-21 22:34:20 +0000

    joepass.php downloads info of print_pattern_info
    git-svn-id: svn://svn.code.sf.net/p/prechacthis/code/trunk/src@436 a36cb7eb-b833-0410-929d-9d6c17de6d6e

--- joepass.php
+++ joepass.php
@@ -1,8 +1,31 @@
 <?php
-if (isset($_REQUEST["pattern"])) {
+if (isset($_GET["pattern"]) && isset($_GET["persons"])){
+	
+	if($_GET["swap"]) {
+		$swaplist = $_GET["swap"];
+	}else{
+		$swaplist = "[]";
+	}
+	
+	if($_GET["newswap"]) {
+		$newswap = $_GET["newswap"];
+	} else {
+		$newswap = "[]";
+	}
+	
+	$plquery  = "swipl -q "
+	          . "-f " . dirname($_SERVER["SCRIPT_FILENAME"]) . "/pl/siteswap.pl "
+	          . "-g \"print_pattern_info("
+	          . $_GET["pattern"] . ", "
+	          . $_GET["persons"] . ", "
+			  . $swaplist . ", "
+			  . $newswap . ", "
+			  . "''"
+	          . "), halt.\"";
+
     header("Content-type: application/force-download");
     header("Content-Transfer-Encoding: Binary");
     header("Content-disposition: attachment; filename=\"".$_REQUEST["pattern"].".pass\"");
-    echo("test");
+	echo `$plquery`;
 }
 ?>
