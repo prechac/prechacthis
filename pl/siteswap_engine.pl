535995f3a2259c8613cada7e6fd819e4ffa40d3a
jjochen <jjochen@a36cb7eb-b833-0410-929d-9d6c17de6d6e>
2008-08-28 09:33:52 +0000

    prechacThis up or down on info side
    documentation started
    
    
    git-svn-id: svn://svn.code.sf.net/p/prechacthis/code/trunk/src@441 a36cb7eb-b833-0410-929d-9d6c17de6d6e

--- pl/siteswap_engine.pl
+++ pl/siteswap_engine.pl
@@ -57,3 +57,42 @@ landingSites2Pattern([Site|LandingSites], [Pos|OneToN], Period, [BaseThrow|BaseP
 	substractUntilNonPositiv(TmpThrow, Period, BaseThrow),
 	landingSites2Pattern(LandingSites, OneToN, Period, BasePattern).
 	
+	
+prechacThis(Pattern, Pos, UpDown, NumberOfJugglers, NewPattern) :-
+	length(Pattern, Period),
+	PreLength is Pos,
+	PostLength is Period - Pos - 1,
+	length(PrePattern, PreLength),
+	length(PostPattern, PostLength),
+	append(PrePattern, _, Pattern),
+	append(_, PostPattern, Pattern),
+	nth0(Pos, Pattern, Throw),
+	prechacThisThrow(Throw, UpDown, NumberOfJugglers, Period, NewThrow),
+	(NewThrow = false ->
+		NewPattern = false;
+		(	
+			length(NewPattern, Period),
+			append(PrePattern, _, NewPattern),
+			append(_, PostPattern, NewPattern),
+			nth0(Pos, NewPattern, NewThrow)
+		)
+	).
+	
+prechacThisThrow(p(Throw, Index, _Origen), up, NumberOfJugglers, Period, p(NewThrow, NewIndex, NewOrigen)) :-
+	Prechator is Period rdiv NumberOfJugglers,
+	NewThrow is Throw + Prechator,
+	NewIndex is (Index + 1) mod NumberOfJugglers,
+	(NewIndex = 0 ->
+		NewOrigen = NewThrow;
+		NewOrigen is NewThrow + (NumberOfJugglers - NewIndex) * Prechator
+	).
+prechacThisThrow(p(Throw, Index, _Origen), down, NumberOfJugglers, Period, p(NewThrow, NewIndex, NewOrigen)) :-
+	Prechator is Period rdiv NumberOfJugglers,
+	NewThrow is Throw - Prechator,
+	NewIndex is (Index - 1) mod NumberOfJugglers,
+	(NewThrow >= 1; (NewThrow = 0, NewIndex = 0)), !,
+	(NewIndex = 0 ->
+		NewOrigen = NewThrow;
+		NewOrigen is NewThrow + (NumberOfJugglers - NewIndex) * Prechator
+	).
+prechacThisThrow(_Throw, down, _NumberOfJugglers, _Period, false) :- !.
