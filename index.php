dbdb450ff79599b3adc6ae422f542125f8877de4
flowma <flowma@a36cb7eb-b833-0410-929d-9d6c17de6d6e>
2008-08-30 05:06:14 +0000

    you can write or instead of ; at objects and periods
    git-svn-id: svn://svn.code.sf.net/p/prechacthis/code/trunk/src@457 a36cb7eb-b833-0410-929d-9d6c17de6d6e

--- index.php
+++ index.php
@@ -33,9 +33,11 @@ if ($_REQUEST){
     echo "<table align='center' cellpadding='0'><tr><td>";
 
 	if (!$_GET["objects"]){$_GET["objects"] = "3;4;5;6;7";}
+	$_GET["objects"] = str_replace("or", ";", $_GET["objects"]);
 	$objects = explode(";", $_GET["objects"]);
 	
-	if (!$_GET["lengths"]){$_GET["lengths"] = "2;3;4;5;6";}
+	if (!$_GET["lengths"]){$_GET["lengths"] = "2;3;4;5;6;8";}
+	$_GET["lengths"] = str_replace("or", ";", $_GET["lengths"]);
 	$lengths = explode(";", $_GET["lengths"]);
 	
 	if ($_GET["persons"]){echo "<h2>" . $_GET["persons"] . " persons</h2>";}
