7cd533faf749b052a9f52561b45516d95abce343
jjochen <jjochen@a36cb7eb-b833-0410-929d-9d6c17de6d6e>
2008-12-09 17:19:15 +0000

    moved so/dll to lib
    git-svn-id: svn://svn.code.sf.net/p/prechacthis/code/branches/pl_server@570 a36cb7eb-b833-0410-929d-9d6c17de6d6e

--- pl/http_server.pl
+++ pl/http_server.pl
@@ -19,15 +19,15 @@ stop_server(Port) :-
 	http_stop_server(Port, []), 
 	halt.
 
-:- dynamic 
+:- dynamic
 	constraintChecked/1,
 	href_type/1,
 	dataResult/2.
 
-
-
-file_search_path(foreign, Path) :-
-	server_location(Path).
+%working_directory(P, P), asserta(library_directory(P)).
+file_search_path(foreign, LibPath) :-
+	working_directory(ServerPath, ServerPath),
+	atom_concat(ServerPath, 'lib/', LibPath).
 
 % ------ pages ------ %
 
@@ -45,46 +45,54 @@ http_joepass_page_path('/joepass.php').
 :- http_handler('/swap', swap_page, []).
 
 
+http_prefix_handler(Prefix, Pred, Options) :-
+	current_prolog_flag(version, Version), 
+	Version > 50661, !,
+	http_handler(Prefix, Pred, [prefix|Options]).
+http_prefix_handler(Prefix, Pred, Options) :-
+	http_handler(prefix(Prefix), Pred, Options).
+
+http_directory(css).
+http_directory(images).
+http_directory(js).
+
+prechacthis_server_type(resource) :-
+	recorded(prechacthis_server_type, resource), !.
+prechacthis_server_type(file).
+	
+:- forall(
+		http_directory(Dir), 
+		(
+			prechacthis_server_type(ServerType),
+			concat_atom(['/', Dir, '/'], Prefix),
+			http_prefix_handler(Prefix, serve_file(ServerType, Dir), [])
+		)
+	).
+
+
+
 
 % ------ resources ------ %
 
-resource(Name, css, css(FileName)) :-
+resource(Name, css, File) :- resource_dir(Name, css, File).
+resource(Name, js, File) :- resource_dir(Name, js, File).
+resource(Name, images, File) :- resource_dir(Name, images, File).
+
+resource_dir(Name, Type, FileLocation) :-
 	atom(Name), !,
-	file_name_extension(Name, css, FileName).
-resource(Name, css, File) :-
+	resource_type_extention(Type, Ext),
+	file_name_extension(Name, Ext, FileName),
+	FileLocation =.. [Type, FileName].
+resource_dir(Name, Type, File) :-
 	var(Name),
-	absolute_file_name(css(''), [file_type(directory)], Dir),
-	file_pattern(Dir, css, Pattern),
+	DirLocation =.. [Type, ''],
+	absolute_file_name(DirLocation, [file_type(directory)], Dir),
+	resource_type_extention(Type, Ext),
+	file_pattern(Dir, Ext, Pattern),
 	expand_file_name(Pattern, Files),
 	member(File, Files),
 	atom_concat(Dir, Local, File),
-	file_name_extension(Name, css, Local).
-
-
-resource(Name, images, images(FileName)) :-
-	atom(Name), !,
-	file_name_extension(Name, png, FileName).
-resource(Name, images, File) :-
-	var(Name),
-	absolute_file_name(images(''), [file_type(directory)], Dir),
-	file_pattern(Dir, png, Pattern),
-	expand_file_name(Pattern, Files),
-	member(File, Files),
-	atom_concat(Dir, Local, File),
-	file_name_extension(Name, png, Local).
-
-resource(Name, js, js(FileName)) :-
-	atom(Name), !,
-	file_name_extension(Name, js, FileName).
-resource(Name, js, File) :-
-	var(Name),
-	absolute_file_name(js(''), [file_type(directory)], Dir),
-	file_pattern(Dir, js, Pattern),
-	expand_file_name(Pattern, Files),
-	member(File, Files),
-	atom_concat(Dir, Local, File),
-	file_name_extension(Name, js, Local).
-
+	file_name_extension(Name, Ext, Local).
 
 
 file_pattern(Dir, Ext, Pattern) :-
@@ -101,9 +109,35 @@ walk_subdirs(Dir, SubSubDir) :-
 	walk_subdirs(SubDirPath, SubSubDir).
 
 
-serve_resource(Type, Request) :-
-	concat_atom(['/', Type, '/'], Prefix),
+resource_content_type(css, 'text/css').
+resource_content_type(images, 'image/png').
+resource_content_type(js, 'text/javascript').
+
+resource_type_extention(css, css).
+resource_type_extention(js, js).
+resource_type_extention(images, png).
+
+
+% ------ files ------ %
+
+server_location(CWD) :-
+	working_directory(CWD, CWD).
+
+
+file_search_path(css, Path) :-
+	server_location(ServerPath),
+	atom_concat(ServerPath, '/css', Path).
+file_search_path(images, Path) :-
+	server_location(ServerPath),
+	atom_concat(ServerPath, '/images', Path).
+file_search_path(js, Path) :-
+	server_location(ServerPath),
+	atom_concat(ServerPath, '/js', Path).
+	
+
+serve_file(resource, Type, Request) :-	
 	memberchk(path(Path), Request),
+	concat_atom(['/', Type, '/'], Prefix),
 	atom_concat(Prefix, Local, Path),
 	resource_type_extention(Type, Ext),
 	file_name_extension(Name, Ext, Local),
@@ -120,124 +154,14 @@ serve_resource(Type, Request) :-
 			fail
 		)
 	).
-	
-resource_content_type(css, 'text/css').
-resource_content_type(images, 'image/png').
-resource_content_type(js, 'text/javascript').
-
-resource_type_extention(css, css).
-resource_type_extention(js, js).
-resource_type_extention(images, png).
-
-
-% ------ files ------ %
-
-server_location(CWD) :-
-	working_directory(CWD, CWD).
-
-	
-%%------ css ------ %%
-
-
-http_prefix_handler(Prefix, Pred) :-
-	current_prolog_flag(version, Version), 
-	Version > 50655, !,
-	http_handler(Prefix, Pred, [prefix]).
-http_prefix_handler(Prefix, Pred) :-
-	http_handler(prefix(Prefix), Pred, []).
-
-
-file_search_path(css, Path) :-
-	server_location(ServerPath),
-	atom_concat(ServerPath, '/css', Path).
-	
-
-:- http_prefix_handler('/css/', serve_resource(css)).
-
-serve_css_file(Request) :-
+serve_file(file, Type, Request) :-
 	memberchk(path(Path), Request),
-	atom_concat('/css/', Local, Path),
-	absolute_file_name(css(Local), FilePath, [access(read)]),
-	absolute_file_name(css(''), Dir, [file_type(directory)]),
+	concat_atom(['/', Type, '/'], Prefix),
+	atom_concat(Prefix, Local, Path),
+	FileLocation =.. [Type, Local],
+	DirLocation =.. [Type, ''],
+	absolute_file_name(FileLocation, FilePath, [access(read)]),
+	absolute_file_name(DirLocation, Dir, [file_type(directory)]),
 	sub_atom(FilePath, 0, _, _, Dir),
 	http_reply_file(FilePath, [], Request).
 
-serve_css_resource(Request) :-
-	memberchk(path(Path), Request),
-	atom_concat('/css/', Local, Path),
-	file_name_extension(Name, css, Local),
-	open_resource(Name, css, StreamIn),
-	format('Content-type: text/css~n~n'),
-	%time_file
-	repeat,
-	(at_end_of_stream(StreamIn) ->
-		close(StreamIn), !;
-		(
-			read_pending_input(StreamIn, Chars, []),
-			format('~s', [Chars]),
-			fail
-		)
-	).
-	
-%%------ images ------ %%
-
-file_search_path(images, Path) :-
-	server_location(ServerPath),
-	atom_concat(ServerPath, '/images', Path).
-
-:- http_prefix_handler('/images/', serve_resource(images)).
-
-serve_images_file(Request) :-
-	memberchk(path(Path), Request),
-	atom_concat('/images/', Local, Path),
-	absolute_file_name(images(Local), FilePath, [access(read)]),
-	absolute_file_name(images(''), Dir, [file_type(directory)]),
-	sub_atom(FilePath, 0, _, _, Dir),
-	http_reply_file(FilePath, [], Request).
-
-
-serve_images_resource(Request) :-
-	memberchk(path(Path), Request),
-	atom_concat('/images/', Local, Path),
-	file_name_extension(Name, png, Local),
-	open_resource(Name, images, StreamIn),
-	format('Content-type: image/png~n~n'),
-	repeat,
-	(at_end_of_stream(StreamIn) ->
-		close(StreamIn), !;
-		(
-			read_pending_input(StreamIn, Chars, []),
-			format('~s', [Chars]),
-			fail
-		)
-	).
-%%------ js ------ %%
-
-file_search_path(js, Path) :-
-	server_location(ServerPath),
-	atom_concat(ServerPath, '/js', Path).
-
-:- http_prefix_handler('/js/', serve_resource(js)).
-
-serve_js_file(Request) :-
-	memberchk(path(Path), Request),
-	atom_concat('/js/', Local, Path),
-	absolute_file_name(js(Local), FilePath, [access(read)]),
-	absolute_file_name(js(''), Dir, [file_type(directory)]),
-	sub_atom(FilePath, 0, _, _, Dir),
-	http_reply_file(FilePath, [], Request).
-
-serve_js_resource(Request) :-
-	memberchk(path(Path), Request),
-	atom_concat('/js/', Local, Path),
-	file_name_extension(Name, js, Local),
-	open_resource(Name, js, StreamIn),
-	format('Content-type: text/javascript~n~n'),repeat,
-	(at_end_of_stream(StreamIn) ->
-		close(StreamIn), !;
-		(
-			read_pending_input(StreamIn, Chars, []),
-			format('~s', [Chars]),
-			fail
-		)
-	).
--- pl/stand_alone.pl
+++ pl/stand_alone.pl
@@ -6,6 +6,7 @@ build_prechacthis :-
 
 build_prechacthis(Target) :-
 	make,
-	qsave_program(Target, [stand_alone(true), class(runtime), autoload(true), goal(server)]).
+	recorda(prechacthis_server_type, resource),
+	qsave_program(Target, [stand_alone(true), class(runtime), autoload(true), goal(server(4211))]).
 	
 
